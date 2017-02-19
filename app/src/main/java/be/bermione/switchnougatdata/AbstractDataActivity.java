3f29287269ccbf513294a3c14fe3deb9cd2f4b49
Auric Goldfinger <goldfinger.auric@gmail.com>
2017-02-19 08:58:51 +0100

    Detect mobile data state
    - If mobile data is already enabled, don't enable it again
    - If mobile data is already disabled, don't disable it again
    - Add permission `android.permission.ACCESS_NETWORK_STATE` for detection
    - Remove obsolete permission `com.android.launcher.permission.INSTALL_SHORTCUT`

--- app/src/main/java/be/bermione/switchnougatdata/AbstractDataActivity.java
+++ app/src/main/java/be/bermione/switchnougatdata/AbstractDataActivity.java
@@ -1,6 +1,9 @@
 package be.bermione.switchnougatdata;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.stericson.RootShell.RootShell;
@@ -9,29 +12,61 @@ import com.stericson.RootShell.execution.Command;
 import com.stericson.RootShell.execution.Shell;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeoutException;
 
 public abstract class AbstractDataActivity extends Activity {
 
     protected void changeMobileDataState(final boolean enableData) {
 
-        if (RootShell.isAccessGiven()) {
-            try {
-                String dataState = enableData ? "enable" : "disable";
-                final Command dataCommand = new Command(0, "svc data " + dataState);
-                final Shell shell = RootShell.getShell(true);
-                shell.add(dataCommand);
-                shell. close();
+        boolean isMobileDataEnabled = false; // Assume disabled
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        try {
+            final Class cmClass = Class.forName(cm.getClass().getName());
+            final Method method = cmClass.getDeclaredMethod("getMobileDataEnabled");
+            method.setAccessible(true); // Make the method callable
+            // get the setting for "mobile data"
+            isMobileDataEnabled = (Boolean) method.invoke(cm);
+        } catch (Exception e) {
+            // Some problem accessible private API
+            // TODO do whatever error handling you want here
+            Log.e(getClass().getSimpleName(), e.getMessage(), e);
+        }
 
-                Toast.makeText(this, String.format("Mobile data %sd", dataState), Toast.LENGTH_LONG).show();
+        final String newDataState = enableData ? "enable" : "disable";
 
-            } catch (IOException | TimeoutException | RootDeniedException e) {
-                e.printStackTrace();
+        if (enableData && !isMobileDataEnabled || !enableData && isMobileDataEnabled) {
+
+            if (RootShell.isAccessGiven()) {
+                try {
+
+                    final Command dataCommand = new Command(0, "svc data " + newDataState);
+                    final Shell shell = RootShell.getShell(true);
+                    shell.add(dataCommand);
+                    shell.close();
+
+                    final String msg = String.format("Mobile data %sd", newDataState);
+                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+                    Log.i(getClass().getSimpleName(), msg);
+
+                    RootShell.closeAllShells();
+
+                } catch (IOException | TimeoutException | RootDeniedException e) {
+                    e.printStackTrace();
+                }
+            } else if (!RootShell.isRootAvailable()) {
+                final String msg = "Root is not available";
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+                Log.e(getClass().getSimpleName(), msg);
+            } else {
+                final String msg = "You must give me root access";
+                Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+                Log.e(getClass().getSimpleName(), msg);
             }
-        } else if (!RootShell.isRootAvailable()) {
-            Toast.makeText(this, "Root is not available", Toast.LENGTH_LONG).show();
         } else {
-            Toast.makeText(this, "You must give me root access", Toast.LENGTH_LONG).show();
+            final String msg = String.format("Mobile data is already %sd", newDataState);
+            //Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+            Log.i(getClass().getSimpleName(), msg);
         }
 
     }
